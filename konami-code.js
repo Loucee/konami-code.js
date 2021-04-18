9d6db7d8f89a6ff40f4cae107a88f4fe1be403c1
Loucee <54353633+Loucee@users.noreply.github.com>
2021-04-18 17:56:57 +0200

    Update konami-code.js

--- konami-code.js
+++ konami-code.js
@@ -1,12 +1,21 @@
 
 var konamiCode = [ 38, 38, 40, 40, 37, 39, 37, 39, 66, 65 ];
 var currentSequence = [ ];
-var cheatsOn = false;
+var KonamiCode = {
+	cheatsOn: false,
+	onComplete: function() {
+		$("body").append("<div class='kc-cheats'>CHEATS <span class='kc-cheats-on'>ON</span></div>");
+	},
+	onFailed: function() { },
+	onKeyPressed: function(keyCode) {
+		$("body").append(`<div class='kc-key-popup'><div class='kc-key-popup-inner'>${_keyFromCode(keyCode)}</div></div>`);
+	}
+}
 
 $(document).keydown(function(event) {
 	var keyCode = event.keyCode || event.which;
 
-	if (cheatsOn) {
+	if (KonamiCode.cheatsOn) {
 		return;
 	}
 
@@ -15,19 +24,22 @@ $(document).keydown(function(event) {
 
 		if (currentSequence.length == konamiCode.length) {
 			currentSequence = [];
-			cheatsOn = true;
-			
-			$("body").append("<div class='kc-cheats'>CHEATS <span class='kc-cheats-on'>ON</span></div>")
+			KonamiCode.cheatsOn = true;
+
+			KonamiCode.onComplete();
 		}
 
-		$("body").append(`<div class='kc-key-popup'><div class='kc-key-popup-inner'>${keyFromCode(keyCode)}</div></div>`);
+		KonamiCode.onKeyPressed(keyCode);		
 	}
 	else {
+		if (currentSequence.length > 0) {
+			KonamiCode.onFailed();
+		}
 		currentSequence = [];
 	}
 });
 
-function keyFromCode(keyCode) {
+function _keyFromCode(keyCode) {
 	if (keyCode == 38) { // arrow up
 		return '<svg width="15px" height="15px;" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="arrow-up" class="svg-inline--fa fa-arrow-up fa-w-14" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M34.9 289.5l-22.2-22.2c-9.4-9.4-9.4-24.6 0-33.9L207 39c9.4-9.4 24.6-9.4 33.9 0l194.3 194.3c9.4 9.4 9.4 24.6 0 33.9L413 289.4c-9.5 9.5-25 9.3-34.3-.4L264 168.6V456c0 13.3-10.7 24-24 24h-32c-13.3 0-24-10.7-24-24V168.6L69.2 289.1c-9.3 9.8-24.8 10-34.3.4z"></path></svg>';
 	}
